4e5c1791a13d47f85038fc027c68f59d68331879
Mona Mohebbi <mmohebbi@pivotal.io>
2020-06-05 14:27:32 -0700

    Add logic around already being on master branch

--- push.sh
+++ push.sh
@@ -2,9 +2,21 @@
 set -ex
 
 git pull -r
-# make
-git push
 
+var=$(git rev-parse --abbrev-ref HEAD)
+if [[ "$var" == "master" ]]; then
+  printf "It looks like you are committing to master. If this is something that needs to be backported, please make the commit there and then use this script to merge it forward.\n Do you want to continue? [y/N]:"
+
+  read input
+  if [[ "$input" == "y" || "$input" == "Y" ]]; then
+    # make
+    git push
+  fi
+  exit 0
+fi
+
+make
+git push
 printf "Do you want to merge this commit forward to master? [y/N]: "
 
 read input
