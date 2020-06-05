72089d9fdd22ea57b464cb5bec7221652e3d922d
Mona Mohebbi <mmohebbi@pivotal.io>
2020-06-05 15:21:00 -0700

    conflcit part2

--- push.sh
+++ push.sh
@@ -5,7 +5,7 @@ git pull -r
 # make
 git push
 
-printf "Do you really want to merge this commit forward to master? [y/N]: "
+printf "Do you want to merge this commit forward to master? [y/N]: "
 
 read input
 if [[ "$input" == "y" || "$input" == "Y" ]]; then
