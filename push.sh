c0fbccf51c0562f9b1accafdf2c8751954df5852
Sarah Weinstein <sweinstein@pivotal.io>
2020-06-08 11:48:14 -0700

    Clean up script

--- push.sh
+++ push.sh
@@ -3,19 +3,17 @@ set -e
 
 git pull -r
 
-var=$(git rev-parse --abbrev-ref HEAD)
-if [[ "$var" == "master" ]]; then
+branch_name=$(git rev-parse --abbrev-ref HEAD)
+if [[ "$branch_name" == "master" ]]; then
   printf "It looks like you are committing to master. If this is something that needs to be backported, please make the commit there and then use this script to merge it forward.\n Do you want to continue? [y/N]:"
 
   read input
   if [[ "$input" == "y" || "$input" == "Y" ]]; then
-   # make
     git push
   fi
   exit 0
 fi
 
-#make
 git push
 printf "Do you want to merge this commit forward to master? [y/N]: "
 
@@ -24,8 +22,9 @@ if [[ "$input" == "y" || "$input" == "Y" ]]; then
   git rebase origin/master
   git checkout master
   git pull -r
-  git merge v1100
+  git merge $branch_name
+  make clean && make build
   git push
 
-  git checkout v1100
+  git checkout $branch_name
 fi
